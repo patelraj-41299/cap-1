c449c43ca41457b1f75bd02ff46abc96d9a268f3
patelraj-41299 <63939517+patelraj-41299@users.noreply.github.com>
2025-04-28 18:24:21 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -18,15 +18,27 @@ resource "aws_internet_gateway" "main_igw" {
   }
 }
 
-# Create Public Subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"
+# Create Public Subnet 1 (us-east-1a)
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-1"
+  }
+}
+
+# Create Public Subnet 2 (us-east-1b)
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "public-subnet-2"
   }
 }
 
@@ -44,8 +56,14 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# Associate Subnet with Route Table
-resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.public_subnet.id
+# Associate Subnet 1 with Route Table
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Associate Subnet 2 with Route Table
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
