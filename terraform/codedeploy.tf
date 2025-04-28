0bb1b48d729e57682775f5df93832965dcbc7c3f
patelraj-41299 <63939517+patelraj-41299@users.noreply.github.com>
2025-04-28 19:34:11 -0400

    Update codedeploy.tf

--- terraform/codedeploy.tf
+++ terraform/codedeploy.tf
@@ -1,30 +1,17 @@
-# Create CodeDeploy Application
-resource "aws_codedeploy_app" "web_app" {
-  name = "cap-1-codedeploy-app"
+resource "aws_codedeploy_app" "deployment_app" {
+  name = "deployment-app"
   compute_platform = "Server"
 }
 
-# Create CodeDeploy Deployment Group
-resource "aws_codedeploy_deployment_group" "web_app_group" {
-  app_name              = aws_codedeploy_app.web_app.name
-  deployment_group_name = "cap-1-codedeploy-group"
-  service_role_arn      = aws_iam_role.codedeploy_role.arn
+resource "aws_codedeploy_deployment_group" "deployment_group" {
+  app_name               = aws_codedeploy_app.deployment_app.name
+  deployment_group_name  = "deployment-group"
+  service_role_arn       = aws_iam_role.codedeploy_service_role.arn
+
+  autoscaling_groups = [aws_autoscaling_group.app_asg.name]
 
   deployment_style {
-    deployment_type = "IN_PLACE"
-    deployment_option = "WITHOUT_TRAFFIC_CONTROL"
-  }
-
-  ec2_tag_set {
-    ec2_tag_filter {
-      key   = "Name"
-      type  = "KEY_AND_VALUE"
-      value = "web-instance"
-    }
-  }
-
-  auto_rollback_configuration {
-    enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
   }
 }
