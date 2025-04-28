e9d6760262ca01712b00300925082f8dba4454a9
patelraj-41299 <63939517+patelraj-41299@users.noreply.github.com>
2025-04-28 17:41:20 -0400

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd /home/ec2-user/backend
+
+# Kill any existing Node.js process
+pkill node || true
+
+# Move into correct backend directory
+cd /home/ec2-user/cap-1/backend
+
+# Install backend dependencies
 npm install
-node server.js > server.log 2>&1 &
+
+# Start Node.js server in background and save logs
+nohup node server.js > server.log 2>&1 &
