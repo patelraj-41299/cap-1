dddc54402359e3211c02811c98f7ce5dabc7925e
patelraj-41299 <63939517+patelraj-41299@users.noreply.github.com>
2025-04-28 19:21:54 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-# Create Internet Gateway
+# Internet Gateway
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -18,7 +18,11 @@ resource "aws_internet_gateway" "main_igw" {
   }
 }
 
+<<<<<<< HEAD
+# Public Subnet 1 (us-east-1a)
+=======
 # Create Public Subnet 1 (us-east-1a)
+>>>>>>> c449c43ca41457b1f75bd02ff46abc96d9a268f3
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -27,6 +31,8 @@ resource "aws_subnet" "public_subnet_1" {
 
   tags = {
     Name = "public-subnet-1"
+<<<<<<< HEAD
+=======
   }
 }
 
@@ -39,10 +45,23 @@ resource "aws_subnet" "public_subnet_2" {
 
   tags = {
     Name = "public-subnet-2"
+>>>>>>> c449c43ca41457b1f75bd02ff46abc96d9a268f3
   }
 }
 
-# Create Route Table
+# Public Subnet 2 (us-east-1b)
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+# Route Table
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -56,13 +75,20 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+<<<<<<< HEAD
+# Subnet Associations
+=======
 # Associate Subnet 1 with Route Table
+>>>>>>> c449c43ca41457b1f75bd02ff46abc96d9a268f3
 resource "aws_route_table_association" "public_subnet_1_association" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_rt.id
 }
 
+<<<<<<< HEAD
+=======
 # Associate Subnet 2 with Route Table
+>>>>>>> c449c43ca41457b1f75bd02ff46abc96d9a268f3
 resource "aws_route_table_association" "public_subnet_2_association" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
