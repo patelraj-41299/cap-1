fe102d5d27d24c31522c4feb0e84deda28ed8db0
patelraj-41299 <63939517+patelraj-41299@users.noreply.github.com>
2025-04-28 19:37:32 -0400

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,4 +1,21 @@
-# Create Security Group for RDS
+resource "aws_db_instance" "app_db" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql8.0"
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.default.name
+
+  tags = {
+    Name = "simple-db"
+  }
+}
+
 resource "aws_security_group" "rds_sg" {
   name        = "rds-sg"
   description = "Allow MySQL traffic"
@@ -24,30 +41,12 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
-# Create RDS Instance
-resource "aws_db_instance" "app_db" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  name                 = "simpledb"
-  username             = var.db_username
-  password             = var.db_password
-  parameter_group_name = "default.mysql8.0"
-  publicly_accessible  = true
-  skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name = aws_db_subnet_group.default.name
-
-  tags = {
-    Name = "simple-db"
-  }
-}
-
-# Create Subnet Group for RDS
 resource "aws_db_subnet_group" "default" {
   name       = "rds-subnet-group"
-  subnet_ids = [aws_subnet.public_subnet.id]
+  subnet_ids = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
 
   tags = {
     Name = "rds-subnet-group"
