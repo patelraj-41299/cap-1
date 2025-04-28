d5d0ad2c4c8ab100b8680da76199f5938247f7a7
patelraj-41299 <63939517+patelraj-41299@users.noreply.github.com>
2025-04-28 19:36:54 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,4 @@
-output "ec2_public_ip" {
-  value = aws_instance.web_instance.public_ip
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.app_db.endpoint
+output "alb_dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = aws_lb.app_alb.dns_name
 }
