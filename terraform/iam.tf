182c2b0d405c5c51f505bbb0a956b378bba2867a
patelraj-41299 <63939517+patelraj-41299@users.noreply.github.com>
2025-04-28 19:35:03 -0400

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,93 +1,89 @@
-# IAM Role for EC2 Instance (CodeDeploy EC2 role)
-resource "aws_iam_role" "ec2_codedeploy_role" {
-  name = "ec2-codedeploy-role"
+# EC2 Instance Role
+resource "aws_iam_role" "ec2_instance_role" {
+  name = "ec2-instance-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = {
         Service = "ec2.amazonaws.com"
       },
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-# Attach AdministratorAccess policy to EC2 role (full access)
-resource "aws_iam_role_policy_attachment" "ec2_admin_attach" {
-  role       = aws_iam_role.ec2_codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_role_policy_attachment" "ec2_instance_role_attach" {
+  role       = aws_iam_role.ec2_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-# IAM Instance Profile for EC2 to use
+# EC2 Instance Profile
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "ec2-instance-profile"
-  role = aws_iam_role.ec2_codedeploy_role.name
+  role = aws_iam_role.ec2_instance_role.name
 }
 
-# IAM Role for CodeDeploy
-resource "aws_iam_role" "codedeploy_role" {
-  name = "codedeploy-role"
+# CodeDeploy Service Role
+resource "aws_iam_role" "codedeploy_service_role" {
+  name = "codedeploy-service-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = {
         Service = "codedeploy.amazonaws.com"
       },
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-# Attach AdministratorAccess policy to CodeDeploy role
-resource "aws_iam_role_policy_attachment" "codedeploy_admin_attach" {
-  role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_role_policy_attachment" "codedeploy_service_role_policy" {
+  role       = aws_iam_role.codedeploy_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
 
-# IAM Role for CodeBuild
+# CodeBuild IAM Role
 resource "aws_iam_role" "codebuild_role" {
   name = "codebuild-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = {
         Service = "codebuild.amazonaws.com"
       },
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-# Attach AdministratorAccess policy to CodeBuild role
-resource "aws_iam_role_policy_attachment" "codebuild_admin_attach" {
+resource "aws_iam_role_policy_attachment" "codebuild_role_attach" {
   role       = aws_iam_role.codebuild_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-# IAM Role for CodePipeline
+# CodePipeline IAM Role
 resource "aws_iam_role" "codepipeline_role" {
   name = "codepipeline-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = {
         Service = "codepipeline.amazonaws.com"
       },
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-# Attach AdministratorAccess policy to CodePipeline role
-resource "aws_iam_role_policy_attachment" "codepipeline_admin_attach" {
+resource "aws_iam_role_policy_attachment" "codepipeline_role_attach" {
   role       = aws_iam_role.codepipeline_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
