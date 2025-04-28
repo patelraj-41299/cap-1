c708fa3b8e9066bf8605358199936a6f31881e45
patelraj-41299 <63939517+patelraj-41299@users.noreply.github.com>
2025-04-28 19:33:25 -0400

    Update codebuild.tf

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -1,23 +1,23 @@
-# CodeBuild Project
 resource "aws_codebuild_project" "app_build" {
   name          = "cap-1-build"
   description   = "Build project for simple web app"
   service_role  = aws_iam_role.codebuild_role.arn
+
   artifacts {
     type = "CODEPIPELINE"
   }
+
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/standard:5.0"
     type                        = "LINUX_CONTAINER"
-    environment_variables = [
-      {
-        name  = "ENV"
-        value = "dev"
-      }
-    ]
-    privileged_mode = true
+    privileged_mode             = true
+    environment_variable {
+      name  = "ENV"
+      value = "dev"
+    }
   }
+
   source {
     type      = "CODEPIPELINE"
     buildspec = "buildspec.yml"
